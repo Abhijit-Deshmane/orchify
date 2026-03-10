23aceb852f8a251e0d9cba570df7e506e6098599
Abhijit-Deshmane <abhijitdeshmane2006@gmail.com>
2026-03-10 23:53:35 +0530

    chore/inngest-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
-import { caller, getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import Image from "next/image";
-import { Suspense } from "react";
-import { Client } from "./client";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { toast } from "sonner";
 
-export default async function Home() {
-   const queryClient = getQueryClient();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-   void queryClient.prefetchQuery(trpc.getUser.queryOptions());
-  
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Job queued");
+    }
+  }));
+
   return (
-   
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p> loading the data </p>}>
-        <Client/>
-        <LogoutButton/>
-      </Suspense>
-     </HydrationBoundary>
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      protected server component
+      <div>
+      {JSON.stringify(data, null ,2)}
+      </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
+      <LogoutButton />
     </div>
   );
-}
+};
+
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,17 @@
 import prisma from '@/lib/db';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { createTRPCRouter, protectedProcedure } from '../init';
 export const appRouter = createTRPCRouter({
-  getUser: baseProcedure.query(() => {
-      return prisma.user.findMany();
-    }),
+  getWorkflows : protectedProcedure.query(({ctx}) =>{
+    return prisma.workflow.findMany();
+  }),
+  
+  createWorkflow : protectedProcedure.mutation(async({ctx}) =>{
+    const res = await prisma.workflow.create({
+      data :{
+        name :"test-workflow",
+      }
+    })
+  })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
