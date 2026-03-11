966c2108c2db87bb95e1a953bc5640e05afbd001
Abhijit-Deshmane <abhijitdeshmane2006@gmail.com>
2026-03-12 00:01:37 +0530

    ai-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { toast } from "sonner";
 
+
 const Page = () => {
   const trpc = useTRPC();
   const queryClient = useQueryClient();
@@ -19,12 +20,24 @@ const Page = () => {
     }
   }));
 
+  const testAi = useMutation(trpc.testAi.mutationOptions({
+    onSuccess : () => {
+      toast.success("ai job is in process")
+    } ,
+    onError : () => (
+      toast.error("something went wrong")
+    )
+  }))
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
       <div>
       {JSON.stringify(data, null ,2)}
       </div>
+    <Button disabled = {testAi.isPending} onClick={() => testAi.mutate()}>
+      test the ai 
+    </Button>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         Create workflow
       </Button>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,28 @@
+import { inngest } from '@/inngest/client';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
-import { createTRPCRouter, protectedProcedure } from '../init';
+
 export const appRouter = createTRPCRouter({
-  getWorkflows : protectedProcedure.query(({ctx}) =>{
+  testAi: baseProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    });
+
+    return { success: true, message: "Job queued" }
+  }),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
-  
-  createWorkflow : protectedProcedure.mutation(async({ctx}) =>{
-    const res = await prisma.workflow.create({
-      data :{
-        name :"test-workflow",
-      }
-    })
-  })
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "antonio@mail.com",
+      },
+    });
+
+    return { success: true, message: "Job queued" }
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
